64e2012c98de7d0164ec4705abe6d448086eba8a
gustavoalisson <gustavoalisson112@gmail.com>
2022-10-18 23:57:08 -0300

    entendendo conceito de marcacoes

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,8 +1,14 @@
+from pytest import mark
 from codigo.jogo import brincadeira
 
+#LINHA DE COMANDO
 # pytest -v (mostra os testes que passou)
 # pytest -x (para no teste que falhou, sem precisar mostrar todos os teste)
 # pýtest --pdb (Debug / Consigo ver o escopo (acesso a tudo que tem no escopo))
+# pytest -k "exemplo_queijo"
+# pytest -s mostra os prints da funcao 
+
+# gerar xml > pytest --junitxml example.xml
 
 '''
 O teste é formado por 3 etapas (GWT - AAA):
@@ -30,6 +36,13 @@ s pulou
 # ! Toda vez que tiver um teste a mensagem não for AssertionError, significa que o que esta errado é a estrutura do codigo
 # OU seja, ASSERTION ERROR - TUDO deu certo, menos o resultado...
 
+# ! MARCAÇÕES
+
+# Criar grupo de marcações exe: @mark.tag 
+# Exemplo: pytest -m tag
+# Exemplo2: pytest -m "not tag"
+
+@mark.smoke
 def test_quando_brincadeira_receber_1_entao_deve_retornar_1():
     '''
     - Brincadeira - SUT - System Under Test
@@ -46,11 +59,25 @@ def test_quando_brincadeira_receber_2_entao_deve_retornar_2():
     
     assert brincadeira(2) == 2
     
-
+@mark.smoke
 def test_quando_brincadeira_receber_3_entao_deve_retornar_queijo():
     
     assert brincadeira(3) == 'queijo'
-    
+
+@mark.goiabada
 def test_quando_brincadeira_receber_5_entao_deve_retornar_goiabada():
     
-    assert brincadeira(5) == 'goiabada'        
+    assert brincadeira(5) == 'goiabada'
+    
+@mark.goiabada
+def test_quando_brincadeira_receber_10_entao_deve_retornar_goiabada():
+    
+    assert brincadeira(10) == 'goiabada'
+
+@mark.goiabada
+@mark.smoke
+def test_quando_brincadeira_receber_20_entao_deve_retornar_goiabada():
+    
+    assert brincadeira(20) == 'goiabada'    
+
+
